9e5fc93f09de69de3587272bd122f848352f1dcc
frederik.jatzkowski <frederik.jatzkowski@check24.de>
2025-10-25 23:37:53 +0200

    improved code style

--- example_full_test.go
+++ example_full_test.go
@@ -6,16 +6,20 @@ import (
 	"github.com/frederik-jatzkowski/errors"
 )
 
-var (
-	ErrTest = errors.New("test error")
-)
+var ErrTest = errors.New("test error")
 
 func Example() {
 	err := errors.Errorf("an error occurred: %w", ErrTest)
 	fmt.Println(err)
 	fmt.Println("can check for wrapped sentinel errors:", errors.Is(err, ErrTest))
-	fmt.Println("sentinel error has no stack trace:", fmt.Sprintf("%+v", ErrTest) == fmt.Sprintf("%s", ErrTest))
-	fmt.Println("wrapped error has a stack trace:", fmt.Sprintf("%+v", err) != fmt.Sprintf("%s", err))
+	fmt.Println(
+		"sentinel error has no stack trace:",
+		fmt.Sprintf("%+v", ErrTest) == fmt.Sprintf("%s", ErrTest),
+	)
+	fmt.Println(
+		"wrapped error has a stack trace:",
+		fmt.Sprintf("%+v", err) != fmt.Sprintf("%s", err),
+	)
 
 	extraErr := errors.Errorf("extra: %w", err)
 
